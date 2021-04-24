c8064c02bca0dedadba5ad4cf46b229085bd7e75
Christian Boyle <mail@christianboyle.com>
2021-04-24 08:40:55 -0500

    Add with-axios example

---
+++ with-axios/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './style.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/christianboyle/repos`;
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>My Repositories</h1>
+      </div>
+      <div className="repo-container">
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className="footer">
+          Built with{' '}
+          <span role="img" aria-label="love">
+            💚
+          </span>{' '}
+          by Christian Boyle
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
